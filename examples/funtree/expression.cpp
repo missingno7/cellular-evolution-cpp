5ae36f2778db02d4ae706314305880776643b0e6
jiri <jiri.vestfal@fetch.ai>
2020-12-03 13:50:01 +0000

    Function implemented

--- examples/funtree/expression.cpp
+++ examples/funtree/expression.cpp
@@ -6,6 +6,7 @@
 #include "constant.cpp"
 #include "tree_variable.cpp"
 #include "operation.cpp"
+#include "function.cpp"
 
 #include "cellular-evolution/utilities/random.h"
 
@@ -23,13 +24,26 @@ Expression::~Expression() {
 
 Expression *Expression::makeRandom(Random &rnd, int min_depth, int max_depth, int current_depth) {
 
+
     if (current_depth < min_depth) {
-        return new Operation(rnd.nextInt(0, Operation::n_types - 1),
-                             makeRandom(rnd, min_depth, max_depth, current_depth + 1),
-                             makeRandom(rnd, min_depth, max_depth, current_depth + 1));
+
+        int val = rnd.nextInt(0, 1);
+
+        switch (val) {
+            case 0:
+                return new Operation(rnd.nextInt(0, Operation::n_types - 1),
+                                     makeRandom(rnd, min_depth, max_depth, current_depth + 1),
+                                     makeRandom(rnd, min_depth, max_depth, current_depth + 1));
+            case 1:
+                return new Function(rnd.nextInt(0, Function::n_types - 1),
+                                    makeRandom(rnd, min_depth, max_depth, current_depth + 1));
+
+        }
+
+
     } else if (current_depth < max_depth) {
 
-        int val = rnd.nextInt(0, 2);
+        int val = rnd.nextInt(0, 3);
 
         switch (val) {
             case 0:
@@ -40,6 +54,9 @@ Expression *Expression::makeRandom(Random &rnd, int min_depth, int max_depth, in
                 return new TreeVariable(rnd.nextBoolean());
             case 2:
                 return new Constant(rnd.nextFloat(-10, 10));
+            case 3:
+                return new Function(rnd.nextInt(0, Function::n_types - 1),
+                                    makeRandom(rnd, min_depth, max_depth, current_depth + 1));
         }
 
     } else {
@@ -58,7 +75,11 @@ Expression *Expression::makeRandom(Random &rnd, int min_depth, int max_depth, in
 void Expression::mutate(Expression *&exp, Random &rnd) {
     float CHANGE_PROB = 0.1;
     float REPLACE_PROB = 0.1;
-    float MUTATE_PROB = 0.1;
+
+    // Change within same type
+    float MUTATE_PROB = 0.3;
+
+    // Switch left and right branches
     float SWITCH_PROB = 0.1;
 
     bool left_new = false;
@@ -68,12 +89,10 @@ void Expression::mutate(Expression *&exp, Random &rnd) {
         exp->mutate_value(rnd);
     }
 
-    if (rnd.nextFloat(0.0, 1.0) < SWITCH_PROB) {
-        if (exp->_left != nullptr && exp->_right != nullptr) {
-            Expression *tmp = exp->_right;
-            exp->_right = exp->_left;
-            exp->_left = tmp;
-        }
+    if (exp->_left != nullptr && exp->_right != nullptr && rnd.nextFloat(0.0, 1.0) < SWITCH_PROB) {
+        Expression *tmp = exp->_right;
+        exp->_right = exp->_left;
+        exp->_left = tmp;
     }
 
     if (rnd.nextFloat(0.0, 1.0) < CHANGE_PROB) {
@@ -84,7 +103,7 @@ void Expression::mutate(Expression *&exp, Random &rnd) {
         exp->_right = nullptr;
         delete exp;
 
-        int val = rnd.nextInt(0, 2);
+        int val = rnd.nextInt(0, 3);
 
         switch (val) {
 
@@ -130,6 +149,39 @@ void Expression::mutate(Expression *&exp, Random &rnd) {
                 }
                 exp = new Constant(rnd.nextFloat(-10, 10));
 
+                break;
+            }
+
+                // Function
+            case 3: {
+                Expression *new_node;
+                if (left == nullptr) {
+                    if (right == nullptr) {
+                        // Both null = make new
+                        new_node = makeRandom(rnd, 0, 2, 0);
+                        left_new = true;
+                    } else {
+                        // Only right not null - use right
+                        new_node = right;
+                    }
+                } else {
+                    if (right == nullptr) {
+                        // Only left not null - use left
+                        new_node = left;
+                    } else {
+                        // both not null, use one, delete other
+                        if (rnd.nextBoolean()) {
+                            new_node = right;
+                            delete left;
+                        } else {
+                            new_node = left;
+                            delete right;
+                        }
+                    }
+                }
+
+                exp = new Function(rnd.nextInt(0, Function::n_types - 1),
+                                   new_node);
                 break;
             }
         }
---
+++ examples/funtree/function.cpp
@@ -0,0 +1,39 @@
+//
+// Created by jiri on 02/12/2020.
+//
+
+#include "function.h"
+
+
+Function::Function(__int8_t operation_type, Expression *left) {
+    _left = left;
+
+    assert (operation_type >= 0 && operation_type < n_types);
+
+    _type = operation_type;
+}
+
+float Function::evaluate(float x) {
+
+    switch (_type) {
+        case 0:
+            return abs(_left->evaluate(x));
+    }
+}
+
+Expression *Function::clone() {
+    return new Function(_type, _left->clone());
+}
+
+std::string Function::toString() {
+    switch (_type) {
+        case 0:
+            return "|" + _left->toString() + "|";
+    }
+
+}
+
+void Function::mutate_value(Random &rnd)
+{
+    _type = rnd.nextInt(0,n_types-1);
+}
